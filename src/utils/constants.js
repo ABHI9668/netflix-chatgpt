b67d9052cde12a5b3d32e57aa3d692821cc8ac2a
Abhishek Singh <as6867909@gmail.com>
2023-09-23 20:40:31 +0530

    remove Open AI key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -21,4 +21,4 @@ export const SUPPORTED_LANGUAGES=[
 
 export const BG_IMAGE="https://assets.nflxext.com/ffe/siteui/vlv3/42df4e1f-bef6-499e-87ff-c990584de314/5e7c383c-1f88-4983-b4da-06e14c0984ba/IN-en-20230904-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 
-export const OPENAI_GPT_KEY="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"
+export const OPENAI_GPT_KEY=""
